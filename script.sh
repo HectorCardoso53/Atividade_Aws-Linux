0e41851c0eb97080644404d603385fbd2aa0534c
Hector de Araújo Cardoso <118605794+HectorCardoso53@users.noreply.github.com>
2024-04-13 13:10:37 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,10 +1,13 @@
-#!/bin/bash
+#!/bin/bash  * interpretador de comandos a ser utilizado é o Bash.
 
-service_name="Apache"
-timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+service_name="Apache" # contém o nome do serviço, neste caso, Apache.
+current_date=$(date +"%Y-%m-%d") # Comando para obter a data atual
+current_time=$(date +"%H:%M:%S") # Comando para obter a hora atual
 
-	if systemctl is-active httpd; then
-		echo "Data/Hora: $timestamp - $service_name - Serviço HTTPD - Status: O serviço está online" >> "/mnt/efs/Hector/httpd-online.txt"
-	else
-		echo "Data/Hora: $timestamp - $service_name - Serviço HTTPD - Status: O serviço está offline" >> "/mnt/efs/Hector/httpd-offline.txt"
-	fi
+if systemctl is-active httpd; then  #Este é um comando verifica se o serviço Apache (httpd) está ativo.
+    echo "Data: $current_date Hora: $current_time - $service_name - Serviço HTTPD - Status: O serviço está online" >> "/mnt/efs/Hector/httpd-online.txt" #Se o serviço Apache estiver ativo, se não
+else
+    echo "Data: $current_date Hora: $current_time - $service_name - Serviço HTTPD - Status: O serviço está offline" >> "/mnt/efs/Hector/httpd-offline.txt" #Se o serviço Apache estiver inativo
+fi #Esta linha fecha o bloco do comando, indicando o fim do comando a ser executado caso o serviço Apache esteja ou não esteja ativo.
+
+
